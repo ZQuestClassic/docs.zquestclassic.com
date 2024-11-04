06de03342be32aaa26c128a3aaaac81a8142683e
Connor Clark <cjamcl@gmail.com>
2024-11-04 00:23:24 -0800

    fix search styling

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ set -e
 
 ZC_DIR="${1:-$HOME/code/ZQuestClassic}"
 
-rsync -r --delete --exclude _headers "$ZC_DIR/docs-www/build/html/" www/
+rsync -r --delete --exclude _headers --exclude _static/scripts/fontawesome.js "$ZC_DIR/docs-www/build/html/" www/
