a21192a52f8b47170bad9d84204465d47d6ef7bb
Connor Clark <cjamcl@gmail.com>
2025-12-08 16:13:27 -0800

    update

--- update.sh
+++ update.sh
@@ -4,5 +4,6 @@ set -e
 
 ZC_DIR="${1:-$HOME/code/ZQuestClassic}"
 
+rm -fr "$ZC_DIR/docs-www/build/html/"
 bash "$ZC_DIR/docs-www/update.sh"
 rsync -r --delete --exclude _headers --exclude _static/scripts/fontawesome.js "$ZC_DIR/docs-www/build/html/" www/
